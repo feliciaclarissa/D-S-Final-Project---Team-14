728e38e72530559092dc8f0b9172d43e359be187
Nandan Doddihal <nadoddi@iu.edu>
2019-10-15 19:06:36 -0400

    API for member creation and display

---
+++ app/public/api/member/create.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use Ramsey\Uuid\Uuid;
+
+$guid = Uuid::uuid4()->toString();
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();//getConnection belongs to singleton class.
+//it returns the same connection everytime I instantiate
+//PDO: Persistent Data object
+
+// Step 2: Create & run the query
+$stmt = $db->prepare('INSERT INTO member (memberID, firstName, lastName, dob, gender, radioNumber, stationNumber, email, position, phoneNumber, address, startDate, isActive)
+                      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+                    );
+$stmt->execute([
+  $guid,
+  $_POST ['firstName'],
+  $_POST ['lastName'],
+  $_POST ['dob'],
+  $_POST ['gender'],
+  $_POST ['radioNumber'],
+  $_POST ['stationNumber'],
+  $_POST ['email'],
+  $_POST ['position'],
+  $_POST ['phoneNumber'],
+  $_POST ['address'],
+  $_POST ['startDate'],
+  $_POST ['isActive']
+]);
+
+// TODO: Error checking?!
+
+//Step 3:
+header('HTTP/1.1 303 See Other');
+header('Location: ../members/?guid='.$guid);
---
+++ app/public/api/member/fetch.php
@@ -0,0 +1,30 @@
+<?php
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();//getConnection belongs to singleton class.
+//it returns the same connection everytime I instantiate
+//PDO: Persistent Data object
+
+// Step 2: Create & run the query
+if(isset($_GET['guid'])){
+  $stmt = $db->prepare('SELECT * FROM member where memberID = ?');
+$stmt->execute([$_GET['guid']]); //execute method of PDO object
+}
+else {
+$stmt = $db->prepare('SELECT * FROM member');
+$stmt->execute(); //execute method of PDO object
+}
+$members = $stmt->fetchAll();
+
+// patientGuid VARCHAR(64) PRIMARY KEY,
+// firstName VARCHAR(64),
+// lastName VARCHAR(64),
+// dob DATE DEFAULT NULL,
+// sexAtBirth CHAR(1) DEFAULT ''
+
+// Step 3: Convert to JSON
+$json = json_encode($members, JSON_PRETTY_PRINT); // a json serialized repersentation of a variable
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
